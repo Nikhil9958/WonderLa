c8c8ac8a17e3d857c5645172f7d3e071065bcfad
Nikhil9958 <aroranikhil987@gmail.com>
2024-10-03 17:04:17 +0530

    Edit Route added

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.set("views",path.join(__dirname,"views"));
 
 app.use(express.urlencoded({extended:true}));
 
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'))
+
 main().then(()=>{
     console.log("Connected to DB");
 }).catch(err=>{
@@ -39,6 +42,7 @@ app.get('/listings/new',(req,res)=>{  //new route
 app.get('/listings/:id',async(req,res)=>{  //show route
     const {id} = req.params;
     const listing = await Listing.findById(id);
+    // console.log(listing);
     res.render("./listings/show.ejs",{listing})
 })
 
@@ -50,12 +54,27 @@ app.post('/listings',async(req,res)=>{
         price:price,
         location:location,
         country:country,
-        image:image
+        image:{url:image.url}
     });
     console.log(newListing);
     await newListing.save();
     console.log("new listing saved");
     res.redirect('/listings');
+})
+
+app.get('/listings/:id/edit',async (req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    // console.log(listing);
+    res.render("./listings/edit.ejs",{listing})
+})
+
+app.put('/listings/:id',async (req,res)=>{
+    const {id}=req.params;
+    const listing = req.body.listing;
+    // console.log(listing);
+    await Listing.findByIdAndUpdate(id,{title:listing.title,description:listing.description,image:{url:listing.url},price:listing.price,country:listing.country,location:listing.location},{new:true})
+    res.redirect(`/listings/${id}`);
 })
 
 
--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,6 @@ const listingSchema = new mongoose.Schema({
         required: true,
     },
     image: {
-        type:String,
-        default:"https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-
         filename: {
             type:String,
             default:"listingimage"
@@ -22,7 +18,11 @@ const listingSchema = new mongoose.Schema({
             type:String,
             default: "https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             set: (v) => v === "" ? "https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-        }
+        },
+        // type:String,
+        // default:"https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        // set: (v) => v === "" ? "https://images.unsplash.com/photo-1727179468848-94adac458b2a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+
     },
     price: {
         type: Number,
